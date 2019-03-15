45ad32a4d721ff92fa918563adb6b096ac26cfef
lieby weintraub <liebahinda@gmail.com>
2019-03-15 04:07:29 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,7 @@ class Anagram
   def match(words_array)
     new_array=[]
     words_array.each do |words| 
-   if  words.split("").sort == @word.split("").sort
+   if  words.split("").sort == @word.split("").sort 
+     new_array
     
 end 
